4cc74c92aa555454be9a664569d35cf5ba05379d
Gabriele De Nardis <gabrieleden83@gmail.com>
2016-09-30 20:40:44 +0200

    Application window drag and drop

--- browser_cache_analyzer.py
+++ browser_cache_analyzer.py
@@ -15,3 +15,38 @@ class BrowserCacheAnalyzer(QtGui.QMainWindow, python_converted_gui.Ui_AnalyzerMa
 
         # Setting up the application user interface from python converted gui
         self.setupUi(self)
+
+    #######################
+    # SECTION: ATTRIBUTES #
+    #######################
+
+        # Mouse cursor coordinates on left click over the application window
+        self.mouse_press_position = None
+
+    ######################################################################
+    # SECTION: MOUSE METHODS OVERRIDE (Application window drag and drop) #
+    ######################################################################
+
+    def mousePressEvent(self, event):
+        """
+        Override for QtGui.QWidget.mousePressEvent to calculate mouse position at click.
+        Event position is relative to the application window.
+        :param event: QtGui.QMouseEvent
+        :return:
+        """
+
+        # Mouse cursor coordinates relative to the application window
+        self.mouse_press_position = event.pos()
+
+    def mouseMoveEvent(self, event):
+        """
+        Override for QtGui.QWidget.mouseMoveEvent to drag the application window.
+        Event buttons indicates the button state when the event was generated.
+        Event position is the global position of the mouse cursor at the time of the event.
+        :param event: QtGui.QMouseEvent
+        :return:
+        """
+
+        # Application window move (with mouse left button)
+        if event.buttons() == QtCore.Qt.LeftButton:
+            self.move(event.globalPos() - self.mouse_press_position)
