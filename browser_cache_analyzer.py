f5aad403da0d42196b370024c8a6b1ff97190215
Gabriele De Nardis <gabrieleden83@gmail.com>
2016-10-01 19:30:10 +0200

    Browser choice screen (System info section)

--- browser_cache_analyzer.py
+++ browser_cache_analyzer.py
@@ -4,6 +4,9 @@
 # PyQt4 imports
 from PyQt4 import QtGui, QtCore
 
+# Python imports
+import platform
+
 # Project imports
 from gui import python_converted_gui
 
@@ -15,6 +18,24 @@ class BrowserCacheAnalyzer(QtGui.QMainWindow, python_converted_gui.Ui_AnalyzerMa
 
         # Setting up the application user interface from python converted gui
         self.setupUi(self)
+
+    ##########################################
+    # SECTION: APPLICATION ELEMENTS SETTINGS #
+    ##########################################
+
+        # "System info" group box elements
+        self.line_system_os_name.setStyleSheet("* {background-color: transparent; }")
+        self.line_system_os_name.setReadOnly(True)
+        self.line_system_os_name.setFrame(False)
+        self.line_system_release.setStyleSheet("* {background-color: transparent; }")
+        self.line_system_release.setReadOnly(True)
+        self.line_system_release.setFrame(False)
+        self.line_system_release_version.setStyleSheet("* {background-color: transparent; }")
+        self.line_system_release_version.setReadOnly(True)
+        self.line_system_release_version.setFrame(False)
+        self.line_system_hostname.setStyleSheet("* {background-color: transparent; }")
+        self.line_system_hostname.setReadOnly(True)
+        self.line_system_hostname.setFrame(False)
 
     #######################
     # SECTION: ATTRIBUTES #
@@ -31,12 +52,44 @@ class BrowserCacheAnalyzer(QtGui.QMainWindow, python_converted_gui.Ui_AnalyzerMa
         self.button_application_minimize.clicked.connect(self.showMinimized)
         self.button_application_close.clicked.connect(self.close_application)
 
+        # Other application elements
+        self.button_welcome_screen_next.clicked.connect(self.set_browser_choice_screen)
+
     ###########################
     # SECTION: WELCOME SCREEN #
     ###########################
 
         # Setting "welcome screen" as application start screen
         self.stackedWidget.setCurrentIndex(0)
+
+        # If "welcome screen", "system info" and "selected browser info" group boxes are not visible
+        if self.stackedWidget.currentIndex() == 0:
+            self.groupBox_system_info.setVisible(False)
+            self.groupBox_selected_browser_info.setVisible(False)
+
+    ##################################
+    # SECTION: BROWSER CHOICE SCREEN #
+    ##################################
+
+    def set_browser_choice_screen(self):
+        """
+        Slot for "next" button in "welcome screen".
+        Setting "browser choice screen": stacked widget index = 1, visible "system info" group box with system values,
+        not visible "selected browser" group box and "table found browsers" containing found browsers in the system.
+        :return:
+        """
+
+        # Setting "browser choice screen"
+        self.stackedWidget.setCurrentIndex(1)
+
+        # "System info" group box visible
+        self.groupBox_system_info.setVisible(True)
+
+        # "System info" values
+        self.line_system_os_name.setText(platform.system())
+        self.line_system_release.setText(platform.release())
+        self.line_system_release_version.setText(platform.version())
+        self.line_system_hostname.setText(platform.node())
 
     ##############################
     # SECTION: CLOSE APPLICATION #
